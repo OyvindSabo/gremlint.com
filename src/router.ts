9f8714c2c1aba7b0a8489df466830e68b2d5b258
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-01 14:39:49 +0100

    Install sharp-router and set up routing

---
+++ src/router.ts
@@ -0,0 +1,8 @@
+import createRouter from 'sharp-router';
+
+const router = createRouter({
+  '/': 'Gremlint - Query formatter',
+  '/style-guide': 'Gremlint - Style guide',
+});
+
+export default router;
