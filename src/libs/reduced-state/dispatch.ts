273312bbf7d6c4a6142b500b57d8554d2f3dc54f
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-01 17:40:14 +0100

    Create routines

--- src/libs/reduced-state/dispatch.ts
+++ src/libs/reduced-state/dispatch.ts
@@ -1,3 +1,3 @@
-export const dispatch = (action: string, payload: any) => {
+export const dispatch = (action: string, payload?: any) => {
   window.dispatchEvent(new CustomEvent(action, { detail: payload }));
 };
---
+++ src/store/routines.ts
@@ -0,0 +1,22 @@
+import { dispatch } from '../libs/reduced-state';
+import {
+  SET_QUERY_INPUT,
+  FORMAT_QUERY,
+  SET_INDENTATION,
+  SET_MAX_LINE_LENGTH,
+  SET_SHOULD_PLACE_DOTS_AFTER_LINE_BREAKS,
+} from './actions';
+
+const handleSetQueryInput = () => dispatch(FORMAT_QUERY);
+const handleSetIndentation = () => dispatch(FORMAT_QUERY);
+const handleSetMaxLineLength = () => dispatch(FORMAT_QUERY);
+const handleSetShouldPlaceDotsAfterLineBreaks = () => dispatch(FORMAT_QUERY);
+
+const routines = {
+  [SET_QUERY_INPUT]: handleSetQueryInput,
+  [SET_INDENTATION]: handleSetIndentation,
+  [SET_MAX_LINE_LENGTH]: handleSetMaxLineLength,
+  [SET_SHOULD_PLACE_DOTS_AFTER_LINE_BREAKS]: handleSetShouldPlaceDotsAfterLineBreaks,
+};
+
+module.exports = routines;
