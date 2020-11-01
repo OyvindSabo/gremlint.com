fc1d037dcf77909c763d5294caf23898f513839e
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-01 14:49:33 +0100

    Create styleVariables

---
+++ src/styleVariables.ts
@@ -0,0 +1,11 @@
+const styleVariables = {
+  borderColor: 'lightgray',
+  textColor: 'slategray',
+  inputTextColor: 'darkslategray',
+  highlightedTextColor: 'darkslategray',
+  disabledTextColor: 'lightgray',
+  highlightColor: 'yellowgreen',
+  white: 'white',
+}
+
+export default styleVariables;
