0d24e774e149d57117f6721dd69276f207418a68
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-01 16:24:51 +0100

    Create actions

---
+++ src/store/actions.ts
@@ -0,0 +1,6 @@
+export const SET_QUERY_INPUT                         = 'SET_QUERY_INPUT';
+export const FORMAT_QUERY                            = 'FORMAT_QUERY';
+export const TOGGLE_SHOULD_SHOW_ADVANCED_OPTIONS     = 'TOGGLE_SHOULD_SHOW_ADVANCED_OPTIONS';
+export const SET_INDENTATION                         = 'SET_INDENTATION';
+export const SET_MAX_LINE_LENGTH                     = 'SET_MAX_LINE_LENGTH';
+export const SET_SHOULD_PLACE_DOTS_AFTER_LINE_BREAKS = 'SET_SHOULD_PLACE_DOTS_AFTER_LINE_BREAKS';
