6905ba54151519e4e6318c897ff36cb1472292e7
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-02 23:24:10 +0100

    Declare return types of getStepGroups/utils

--- src/libs/gremlint/formatSyntaxTree/formatTraversal/getStepGroups/utils.ts
+++ src/libs/gremlint/formatSyntaxTree/formatTraversal/getStepGroups/utils.ts
@@ -1,9 +1,9 @@
 import { GremlinSyntaxTree, GremlinTokenType } from '../../../types';
 
-export const isTraversalSource = (step: GremlinSyntaxTree) =>
+export const isTraversalSource = (step: GremlinSyntaxTree): boolean =>
   step.type === GremlinTokenType.Word && step.word === 'g';
 
-export const isModulator = (step: GremlinSyntaxTree) =>
+export const isModulator = (step: GremlinSyntaxTree): boolean =>
   step.type === GremlinTokenType.Method &&
   step.method.type === GremlinTokenType.Word &&
   [
